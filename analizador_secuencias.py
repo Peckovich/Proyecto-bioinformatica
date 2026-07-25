8d91aa7005e651a71646074af021fa2eb755c8a8
hegaba16-alt <hegaba16@gmail.com>
2026-07-24 20:51:09 -0600

    Se crea archivo .py que contendra el código para analizar secuencias

---
+++ analizador_secuencias.py
@@ -0,0 +1,4 @@
+#Estoy experimentando con VSCode instalado en windows pero accediendo desde WSL. 
+""" la idea de este archivo es crear un codigo que pueda analizar secuencias del gen 
+HBB para encontrar la alteracion que provoca la anemia falciforme. Se elige esta enfermedad por ser 
+provocada solo por el cambio en una base nitrogenada""" 
